a20afbbeae526ea0bbae110c8c5cf7848382deab
Lesedi Madumo <LesediM@lombardins.com>
2021-04-21 12:21:22 +0200

    Complete demo

--- images3.py
+++ images3.py
@@ -5,6 +5,7 @@ from tkinter import ttk
 HEIGHT = 1000
 WIDTH = 1000
 
+
 root = Tk()
 root.title('Qouting Tool')
 root.iconbitmap("C:/Users/LesediM/Desktop/New/LI.ico")
@@ -26,12 +27,15 @@ my_canvas.configure(yscrollcommand=my_yscrollbar.set)
 my_canvas.configure(xscrollcommand=my_xscrollbar.set)
 my_canvas.bind('<Configure>',lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
 
-main_frame = Frame(my_canvas)
 
-pol_frame = ttk.Frame(main_frame)
-fleet_frame = ttk.Frame(main_frame)
-cover_frame = ttk.Frame(main_frame)
-specified_frame = ttk.Frame(main_frame)
+main_frame = Frame(my_canvas)
+policyinfo_frame = LabelFrame(main_frame,text = 'Policy Information')
+policyinfo_frame.grid(row = 0,column = 0,sticky = W)
+
+pol_frame = ttk.Frame(policyinfo_frame)
+fleet_frame = ttk.Frame(policyinfo_frame)
+cover_frame = ttk.Frame(policyinfo_frame)
+specified_frame = ttk.Frame(policyinfo_frame)
 
 pol_frame.grid(row = 0, column =0,sticky = W)
 fleet_frame.grid(row = 1, column =0,sticky = W)
@@ -302,17 +306,15 @@ entry_lossofkeys2 = Entry(specified_frame).grid(row = 15, column =1)
 
 
 #---------------------------Rating Info------------------------------
-rating_frame = Frame(main_frame)
-option1_frame = LabelFrame(main_frame,text = 'Option 1')
-option2_frame = LabelFrame(main_frame,text = 'Option 2')
-option3_frame = LabelFrame(main_frame,text = 'Option 3')
-option4_frame = LabelFrame(main_frame,text = 'Option 4')
+rating_frame = LabelFrame(main_frame,text = 'Rating Information')
+rating_frame.grid(row = 1, column =0,sticky = W)
 
-rating_frame.grid(row = 4, column =0,sticky = W)
-option1_frame.grid(row = 5, column =0,sticky = W)
-option2_frame.grid(row = 6, column =0,sticky = W)
-option3_frame.grid(row = 7, column =0,sticky = W)
-option4_frame.grid(row = 8, column =0,sticky = W)
+option1_frame = LabelFrame(rating_frame,text = 'Option 1')
+option2_frame = LabelFrame(rating_frame,text = 'Option 2')
+option3_frame = LabelFrame(rating_frame,text = 'Option 3')
+option4_frame = LabelFrame(rating_frame,text = 'Option 4')
+
+
 
 
 label_ratinginfo = Label(rating_frame,text = 'Rating Information',font = 'times 18 bold underline')
@@ -328,6 +330,10 @@ label_priorclaims.grid(row = 2,column = 0,sticky = W)
 label_totalclaims.grid(row = 3,column = 0,sticky = W)
 label_numofmonths.grid(row = 4,column = 0,sticky = W)
 label_annclaims.grid(row = 5,column = 0,sticky = W)
+option1_frame.grid(row = 7, column =0,sticky = W)
+option2_frame.grid(row = 8, column =0,sticky = W)
+option3_frame.grid(row = 9, column =0,sticky = W)
+option4_frame.grid(row = 10, column =0,sticky = W)
 
 entry_fleetvalue= Entry(rating_frame).grid(row = 1,column = 1,sticky = W)
 entry_proirclaims = Entry(rating_frame).grid(row = 2,column = 1,sticky = W)
@@ -337,7 +343,7 @@ entry_annclaims = Entry(rating_frame).grid(row = 5,column = 1,sticky = W)
 
 label_premcalcs = Label(rating_frame,text = 'Premium Calcuations',font = 'times 18 bold underline')
 label_premcalcs.grid(row = 6,column = 0)
-
+#------------------------Option 1 ---------------------------------
 label_opt1 = Label(option1_frame,text = 'Option 1- Conventional',font = 'times 15 bold underline')
 label_showqoteopt1 = Label(option1_frame,text = 'Show on Quote:' ,font = 'times 12 bold')
 label_premfleetopt1 = Label(option1_frame,text = 'Premiunm based on fleet value:',font = 'times 12 bold')
@@ -347,7 +353,7 @@ label_premlossopt1 = Label(option1_frame,text = 'Premiunm based on loss value:',
 label_roundupeopt1 = Label(option1_frame,text = 'Round-up/-down',font = 'times 12 bold')
 label_roundnearopt1 = Label(option1_frame,text = 'Round to nearest:',font = 'times 12 bold')
 label_premquoteopt1 = Label(option1_frame,text = 'Premium on Quote:',font = 'times 12 bold')
-label_overrideopt1 = Label(option1_frame,text = 'Override?:',font = 'times 12 bold')
+label_overrideopt1 = Label(option1_frame,text = 'Override?',font = 'times 12 bold')
 label_premquote2opt1 = Label(option1_frame,text = 'Premium on Quote:',font = 'times 12 bold')
 
 clicked_typerating = StringVar(option1_frame)
@@ -355,7 +361,7 @@ clicked_typerating.set('No')
 menu_type = OptionMenu(option1_frame, clicked_typerating,'No','Yes')
 menu_type.grid(row = 1,column = 1)
 
-label_opt1.grid(row =0,column = 1)
+label_opt1.grid(row =0,column = 0)
 label_showqoteopt1.grid(row = 1, column = 0,sticky = W)
 label_ratioapplied.grid(row = 2,column = 1)
 label_premiumopt1.grid(row = 2,column = 2)
@@ -363,28 +369,252 @@ label_premfleetopt1.grid(row =3,column = 0,sticky = W)
 label_premlossopt1.grid(row =4,column = 0,sticky = W)
 label_roundupeopt1.grid(row =5,column = 0,sticky = W)
 label_roundnearopt1.grid(row =6,column = 0,sticky = W)
-label_premquote2opt1.grid(row =7,column = 0,sticky = W)
+label_premquoteopt1.grid(row =7,column = 0,sticky = W)
 label_overrideopt1.grid(row =8,column = 0,sticky = W)
-label_premquote2opt1.grid(row =9,column = 1,sticky = W)
+label_premquote2opt1.grid(row =9,column = 0,sticky = W)
 
-entry_fleetpercopt1 = Entry(option1_frame).grid(row = 3,column = 2)
 entry_fleetpremopt1 = Entry(option1_frame).grid(row = 3,column = 1)
-entry_losspercopt1 = Entry(option1_frame).grid(row = 4,column = 2)
+entry_fleetpercopt1 = Entry(option1_frame).grid(row = 3,column = 2)
+
 entry_losspremopt1 = Entry(option1_frame).grid(row = 4,column = 1)
+entry_losspercopt1 = Entry(option1_frame).grid(row = 4,column = 2)
+
+entry_premopt1 =Entry(option1_frame).grid(row = 7,column = 1)
+entry_prem2opt1 =Entry(option1_frame).grid(row = 9,column = 1)
+
+clicked_roundopt1 = StringVar(option1_frame)
+clicked_roundopt1.set('Up')
+menu_roundopt1 = OptionMenu(option1_frame, clicked_roundopt1,'Up','Down')
+menu_roundopt1.grid(row = 5,column = 1)
 
 clicked_typerating = StringVar(option1_frame)
-clicked_typerating.set('No')
-menu_type = OptionMenu(option1_frame, clicked_typerating,'Up','Down')
+clicked_typerating.set('R10 000')
+menu_type = OptionMenu(option1_frame, clicked_typerating,'R1000','R10 000','R100 000')
+menu_type.grid(row = 6,column = 1)
+
+clicked_overopt1 = StringVar(option1_frame)
+clicked_overopt1.set('No')
+menu_overopt1 = OptionMenu(option1_frame, clicked_overopt1,'No','Yes')
+menu_overopt1.grid(row = 8,column = 1)
+
+#------------------------Option 2---------------------------
+label_opt2 = Label(option2_frame,text = 'Option 2- Burning Cost',font = 'times 15 bold underline')
+label_showqoteopt2 = Label(option2_frame,text = 'Show on Quote:' ,font = 'times 12 bold')
+label_loadingopt2 = Label(option2_frame,text = 'Loading: ')
+label_totalpremopt2 = Label(option2_frame,text = 'Total Premium: ')
+
+label_premloadingopt2 = Label(option2_frame,text = 'Premiunm loading',font = 'times 12 bold')
+
+label_premsplitopt2 = Label(option2_frame,text = 'Premium Split: ',font = 'times 12 bold')
+label_prempropopt2 = Label(option2_frame,text = 'Proportional Split: ')
+label_premopt2 = Label(option2_frame,text = 'Premium: ')
+
+label_depositpremopt2 = Label(option2_frame,text = ':Deposit premium ')
+label_burner1opt2 = Label(option2_frame,text = 'Burner 1')
+label_burner2opt2 = Label(option2_frame,text = 'Burner 2')
+
+
+clicked_showquoteopt2 = StringVar(option2_frame)
+clicked_showquoteopt2.set('No')
+menu_type = OptionMenu(option2_frame, clicked_showquoteopt2,'No','Yes')
 menu_type.grid(row = 1,column = 1)
 
-clicked_typerating = StringVar(option1_frame)
-clicked_typerating.set('No')
-menu_type = OptionMenu(option1_frame, clicked_typerating,'No','Yes')
-menu_type.grid(row = 1,column = 1)
+label_opt2.grid(row =0,column = 0)
+label_showqoteopt2.grid(row = 1, column = 0,sticky = W)
+label_loadingopt2.grid(row = 2,column = 1)
+label_totalpremopt2.grid(row = 2,column = 2)
+label_premloadingopt2.grid(row =3,column = 0,sticky = W)
+label_premsplitopt2.grid(row =4,column = 0,sticky = W)
+label_prempropopt2 .grid(row =4,column = 1)
+label_premopt2.grid(row =4,column = 2)
+
+label_depositpremopt2.grid(row =5,column = 0,sticky = W)
+label_burner1opt2.grid(row =6,column = 0,sticky = W)
+label_burner2opt2.grid(row =7,column = 0,sticky = W)
+
+entry_premloadingopt2 = Entry(option2_frame).grid(row = 3,column = 1)
+entry_totalpremopt2 = Entry(option2_frame).grid(row = 3,column = 2)
+entry_depositsplitopt2 = Entry(option2_frame).grid(row = 5,column = 1)
+entry_depositpremopt2 = Entry(option2_frame).grid(row = 5,column = 2)
+entry_burner1splitopt2 = Entry(option2_frame).grid(row = 6,column = 1)
+entry_burner1premopt2 =Entry(option2_frame).grid(row = 6,column = 2)
+ntry_burner2splitopt2 =Entry(option2_frame).grid(row = 7,column = 1)
+entry_burner2premopt2 =Entry(option2_frame).grid(row = 7,column = 2)
 
 
+#--------------------------Option3-------------------------------------------
+label_opt3 = Label(option3_frame,text = 'Option 3- Aggregate Excess ',font = 'times 15 bold underline ')
+label_showquoteopt3 = Label(option3_frame,text = 'Show On Quote: ',font = 'times 12 bold ')
+label_annualopt3 = Label(option3_frame,text = 'Annual Aggregate: ',font = 'times 12 bold ')
+label_applicableopt3 = Label(option3_frame,text = 'Applicable to: ',font = 'times 12 bold ')
+label_stoplossopt3 = Label(option3_frame,text = 'Stop Loss Limit: ',font = 'times 12 bold ')
+label_claimsopt3 = Label(option3_frame,text = 'Claims paid by insurer: ',font = 'times 12 bold')
+label_numofmonthsopt3 = Label(option3_frame,text = 'No. of months before renewal: ',font = 'times 12 bold')
+label_annclaimsopt3 = Label(option3_frame,text = 'Annualised Claims: ',font = 'times 12 bold')
+label_rateopt3 = Label(option3_frame,text = 'Rate/Ratio applied')
+label_premopt3 = Label(option3_frame,text = ' Premium')
+label_premlossopt3 = Label(option3_frame,text = 'Premiium based on loss ratio: ',font = 'times 12 bold')
+label_premclaimsopt3 = Label(option3_frame,text = 'Premium based on claims above stop loss limit: ',font = 'times 12 bold')
+label_roundupopt3 = Label(option3_frame,text = 'Round up/down: ',font = 'times 12 bold')
+label_roundnearopt3 = Label(option3_frame,text = 'Round to nearest: ',font = 'times 12 bold')
+label_premquoteopt3 = Label(option3_frame,text = 'Premium on Quote: ',font = 'times 12 bold ')
+label_overrideopt3 = Label(option3_frame,text = 'Override?: ',font = 'times 12 bold')
+label_premquote2opt3 = Label(option3_frame,text = 'Premium on Quote: ',font = 'times 12 bold')
+label_showtextopt3 = Label(option3_frame,text = 'Show the below text on quote?: ',font = 'times 12 bold')
 
+label_opt3.grid(row = 0,column = 0,sticky = W)
+label_showquoteopt3.grid(row = 1,column = 0,sticky = W)
+label_annualopt3.grid(row = 2,column = 0,sticky = W)
+label_applicableopt3.grid(row = 3,column = 0,sticky = W)
+label_stoplossopt3.grid(row = 4,column = 0,sticky = W)
+label_claimsopt3 .grid(row = 5,column = 0,sticky = W)
+label_numofmonthsopt3 .grid(row = 6,column = 0,sticky = W)
+label_annclaimsopt3.grid(row = 7,column = 0,sticky = W)
+label_rateopt3.grid(row = 8,column = 1,sticky = W)
+label_premopt3.grid(row = 8,column = 2,sticky = W)
+label_premlossopt3.grid(row = 9,column = 0,sticky = W)
+label_premclaimsopt3 .grid(row = 10,column = 0,sticky = W)
+label_roundupopt3.grid(row = 11,column = 0,sticky = W)
+label_roundnearopt3.grid(row = 12,column = 0,sticky = W)
+label_premquoteopt3.grid(row = 13,column = 0,sticky = W)
+label_overrideopt3.grid(row = 14,column = 0,sticky = W)
+label_premquote2opt3.grid(row = 15,column = 0,sticky = W)
+label_showtextopt3.grid(row = 16,column = 0,sticky = W)
 
+clicked_showquoteopt3 = StringVar(option3_frame)
+clicked_showquoteopt3.set('No')
+menu_showopt3 = OptionMenu(option3_frame, clicked_showquoteopt3,'No','Yes')
+menu_showopt3.grid(row = 1,column = 1)
+
+entry_annual = Entry(option3_frame).grid(row = 2,column = 1)
+
+clicked_sectionopt3 = StringVar(option3_frame)
+clicked_sectionopt3.set('Section A & B')
+menu_sectionopt3 = OptionMenu(option3_frame, clicked_sectionopt3,'Section A & B','Section A')
+menu_sectionopt3.grid(row = 3,column = 1)
+
+entry_stoplossopt3 = Entry(option3_frame).grid(row = 4,column = 1)
+entry_claimsopt3= Entry(option3_frame).grid(row = 5,column = 1)
+entry_numofmonthsopt3 = Entry(option3_frame).grid(row = 6,column = 1)
+entry_annclaimsopt3 = Entry(option3_frame).grid(row = 7,column = 1)
+entry_premrateopt3 = Entry(option3_frame).grid(row = 9,column = 1)
+entry_ratiopremopt3 = Entry(option3_frame).grid(row = 9,column = 2)
+entry_claimrateopt3 = Entry(option4_frame).grid(row = 10,column = 1)
+entry_claimpremopt3 = Entry(option4_frame).grid(row = 10,column = 2)
+
+clicked_roundopt3 = StringVar(option3_frame)
+clicked_roundopt3.set('Up')
+menu_roundopt3 = OptionMenu(option3_frame, clicked_roundopt3,'Up','Down')
+menu_roundopt3.grid(row = 11,column = 1)
+
+clicked_sectionopt3 = StringVar(option3_frame)
+clicked_sectionopt3.set('Section A & B')
+menu_shectionopt3 = OptionMenu(option3_frame, clicked_sectionopt3,'R1 000','R10 000','R100 000')
+menu_sectionopt3.grid(row = 12,column = 1)
+
+entry_premonquoteopt3 = Entry(option3_frame).grid(row = 13,column = 1)
+
+clicked_overrideopt3 = StringVar(option3_frame)
+clicked_overrideopt3.set('No')
+menu_roundopt3 = OptionMenu(option3_frame, clicked_overrideopt3,'No','Yes')
+menu_roundopt3.grid(row = 14,column = 1)
+
+entry_premonquote2opt3 = Entry(option3_frame).grid(row = 15,column = 1)
+
+clicked_textopt3 = StringVar(option3_frame)
+clicked_textopt3.set('No')
+menu_roundopt3 = OptionMenu(option3_frame, clicked_overrideopt3,'No','Yes')
+menu_roundopt3.grid(row = 16,column = 1)
+textlabelopt3 = 'All legal, assessor fees and other fees/expenses will contribute the aggregrate deductible'
+label_textopt3 = Label(option3_frame,text = textlabelopt3,font = 'times 11 italic')
+label_textopt3.grid(row = 17,column = 0) 
+
+#-------------------Option 4----------------------------------------------------------
+label_opt4 = Label(option4_frame,text = 'Option 3- Aggregate Excess with Burner ',font = 'times 15 bold underline ')
+label_showquoteopt4 = Label(option4_frame,text = 'Show On Quote: ',font = 'times 12 bold ')
+label_annualopt4 = Label(option4_frame,text = 'Annual Aggregate: ',font = 'times 12 bold ')
+label_applicableopt4 = Label(option4_frame,text = 'Applicable to: ',font = 'times 12 bold ')
+label_stoplossopt4 = Label(option4_frame,text = 'Stop Loss Limit: ',font = 'times 12 bold ')
+label_claimsopt4 = Label(option4_frame,text = 'Claims paid by insurer: ',font = 'times 12 bold')
+label_numofmonthsopt4 = Label(option4_frame,text = 'No. of months before renewal: ',font = 'times 12 bold')
+label_annclaimsopt4 = Label(option4_frame,text = 'Annualised Claims: ',font = 'times 12 bold')
+label_rateopt4= Label(option4_frame,text = 'Rate/Ratio applied')
+label_premopt4 = Label(option4_frame,text = ' Premium')
+label_premlossopt4 = Label(option4_frame,text = 'Premiium based on loss ratio: ',font = 'times 12 bold')
+label_premclaimsopt4 = Label(option4_frame,text = 'Premium based on claims above stop loss limit: ',font = 'times 12 bold')
+label_roundupopt4 = Label(option4_frame,text = 'Round up/down: ',font = 'times 12 bold')
+label_roundnearopt4 = Label(option4_frame,text = 'Round to nearest: ',font = 'times 12 bold')
+label_premquoteopt4 = Label(option4_frame,text = 'Premium on Quote: ',font = 'times 12 bold ')
+label_overrideopt4 = Label(option4_frame,text = 'Override?: ',font = 'times 12 bold')
+label_premquote2opt4 = Label(option4_frame,text = 'Premium on Quote: ',font = 'times 12 bold')
+label_showtextopt4 = Label(option4_frame,text = 'Show the below text on quote?: ',font = 'times 12 bold')
+
+label_opt4.grid(row = 0,column = 0,sticky = W)
+label_showquoteopt4.grid(row = 1,column = 0,sticky = W)
+label_annualopt4.grid(row = 2,column = 0,sticky = W)
+label_applicableopt4.grid(row = 3,column = 0,sticky = W)
+label_stoplossopt4.grid(row = 4,column = 0,sticky = W)
+label_claimsopt4.grid(row = 5,column = 0,sticky = W)
+label_numofmonthsopt4.grid(row = 6,column = 0,sticky = W)
+label_annclaimsopt4.grid(row = 7,column = 0,sticky = W)
+label_rateopt4.grid(row = 8,column = 1,sticky = W)
+label_premopt4.grid(row = 8,column = 2,sticky = W)
+label_premlossopt4.grid(row = 9,column = 0,sticky = W)
+label_premclaimsopt4.grid(row = 10,column = 0,sticky = W)
+label_roundupopt4.grid(row = 11,column = 0,sticky = W)
+label_roundnearopt4.grid(row = 12,column = 0,sticky = W)
+label_premquoteopt4.grid(row = 13,column = 0,sticky = W)
+label_overrideopt4.grid(row = 14,column = 0,sticky = W)
+label_premquote2opt4.grid(row = 15,column = 0,sticky = W)
+label_showtextopt4.grid(row = 16,column = 0,sticky = W)
+
+clicked_showquoteopt4 = StringVar(option3_frame)
+clicked_showquoteopt4.set('No')
+menu_showopt4= OptionMenu(option4_frame, clicked_showquoteopt4,'No','Yes')
+menu_showopt4.grid(row = 1,column = 1)
+
+entry_annual = Entry(option3_frame).grid(row = 2,column = 1)
+
+clicked_sectionopt4 = StringVar(option4_frame)
+clicked_sectionopt4.set('Section A & B')
+menu_sectionopt4 = OptionMenu(option4_frame, clicked_sectionopt4,'Section A & B','Section A')
+menu_sectionopt4.grid(row = 3,column = 1)
+
+entry_stoplossopt4 = Entry(option4_frame).grid(row = 4,column = 1)
+entry_claimsopt4= Entry(option4_frame).grid(row = 5,column = 1)
+entry_numofmonthsopt4 = Entry(option4_frame).grid(row = 6,column = 1)
+entry_annclaimsopt4 = Entry(option4_frame).grid(row = 7,column = 1)
+entry_premrateopt4 = Entry(option4_frame).grid(row = 9,column = 1)
+entry_ratiopremopt4 = Entry(option4_frame).grid(row = 9,column = 2)
+entry_claimrateopt4 = Entry(option4_frame).grid(row = 10,column = 1)
+entry_claimpremopt4 = Entry(option4_frame).grid(row = 10,column = 2)
+
+clicked_roundopt4 = StringVar(option4_frame)
+clicked_roundopt4.set('Up')
+menu_roundopt4 = OptionMenu(option4_frame, clicked_roundopt4,'Up','Down')
+menu_roundopt4.grid(row = 11,column = 1)
+
+clicked_sectionopt4 = StringVar(option3_frame)
+clicked_sectionopt4.set('Section A & B')
+menu_shectionopt4= OptionMenu(option4_frame, clicked_sectionopt4,'R1 000','R10 000','R100 000')
+menu_sectionopt4.grid(row = 12,column = 1)
+
+entry_premonquoteopt4 = Entry(option4_frame).grid(row = 13,column = 1)
+
+clicked_overrideopt4 = StringVar(option4_frame)
+clicked_overrideopt4.set('No')
+menu_roundopt4 = OptionMenu(option4_frame, clicked_overrideopt4,'No','Yes')
+menu_roundopt4.grid(row = 14,column = 1)
+
+entry_premonquote2opt4 = Entry(option4_frame).grid(row = 15,column = 1)
+
+clicked_textopt4 = StringVar(option4_frame)
+clicked_textopt4.set('No')
+menu_roundopt4 = OptionMenu(option4_frame, clicked_overrideopt4,'No','Yes')
+menu_roundopt4.grid(row = 16,column = 1)
+textlabelopt4 = 'All legal, assessor fees and other fees/expenses will contribute the aggregrate deductible'
+label_textopt4 = Label(option4_frame,text = textlabelopt4,font = 'times 11 italic')
+label_textopt4.grid(row = 17,column = 0) 
 
 button_quit = Button(root, text="Exit Program", command=root.quit)
 
